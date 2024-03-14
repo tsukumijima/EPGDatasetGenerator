d1f3ba2ed5a712426e5aba63ee82225294231f80
tsukumi <tsukumijima@users.noreply.github.com>
2024-03-14 23:14:16 +0000

    Update: 手動アノテーション支援ツールを実装 Aivis で実装した時の Gradio コードのロジックを大部分流用した

--- 03-AnnotateEPGDatasetSubset.py
+++ 03-AnnotateEPGDatasetSubset.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
+import gradio
 import jsonlines
-import time
 import typer
 from pathlib import Path
 from typing import Annotated
@@ -9,46 +9,138 @@ from typing import Annotated
 from utils.constants import EPGDatasetSubset
 
 
-def generate_annotations(subset: EPGDatasetSubset) -> EPGDatasetSubset:
-
-    return subset
-
-
 app = typer.Typer()
 
 @app.command()
 def main(
-    subset_path: Annotated[Path, typer.Option(help='アノテーションを手動追加するデータセットのサブセットのパス。', dir_okay=False)] = Path('epg_dataset_subset.jsonl'),
+    subset_path: Annotated[Path, typer.Option(help='データセットのサブセットのパス。', dir_okay=False)] = Path('epg_dataset_subset.jsonl'),
+    annotated_subset_path: Annotated[Path, typer.Option(help='アノテーション追加後のデータセットのサブセットのパス。')] = Path('epg_dataset_subset_annotated.jsonl'),
 ):
     if not subset_path.exists():
         print(f'ファイル {subset_path} は存在しません。')
         return
+    if annotated_subset_path.exists():
+        print(f'ファイル {annotated_subset_path} は既に存在しています。')
+        return
 
-    start_time = time.time()
-
-    print('-' * 80)
-    modified_subsets: list[EPGDatasetSubset] = []
+    typer.echo('=' * 80)
+    print('ロード中...')
+    subsets: list[EPGDatasetSubset] = []
     with jsonlines.open(subset_path, 'r') as reader:
         for obj in reader:
-            subset = EPGDatasetSubset.model_validate(obj)
-            print(f'番組名:   {subset.title}')
-            print(f'番組概要: {subset.description}')
-            modified_subset = generate_annotations(subset)
-            print(f'{subset.id} のアノテーションを追加しました。')
-            print(f'シリーズタイトル: {modified_subset.series_title}')
-            print(f'話数: {modified_subset.episode_number} / サブタイトル: {modified_subset.subtitle}')
-            modified_subsets.append(modified_subset)
-            print('-' * 80)
+            subsets.append(EPGDatasetSubset.model_validate(obj))
+    print(f'ロード完了: {len(subsets)} 件')
+    typer.echo('=' * 80)
 
-    print('-' * 80)
-    print(f'{subset_path} に書き込んでいます...')
-    with jsonlines.open(subset_path, 'w') as writer:
-        for subset in modified_subsets:
-            writer.write(subset.model_dump(mode='json'))
+    with jsonlines.open(annotated_subset_path, 'w') as writer:
 
-    elapsed_time = time.time() - start_time
-    print(f'合計時間: {elapsed_time:.2f} 秒')
-    print('-' * 80)
+        # 現在処理中の EPG データサブセットのインデックス
+        current_index = 0
+
+        def OnClick(
+            id: str,
+            title: str,
+            description: str,
+            series_title: str,
+            episode_number: str,
+            subtitle: str,
+        ) -> tuple[gradio.Textbox, gradio.Textbox, gradio.Textbox, gradio.Textbox, gradio.Textbox, gradio.Textbox]:
+            """ 確定ボタンが押されたときの処理 """
+
+            nonlocal current_index, subsets
+
+            # 初期画面から「確定」を押して実行されたイベントなので、保存処理は実行しない
+            if id == '確定ボタンを押して、データセット作成を開始してください。':
+                typer.echo('=' * 80)
+                typer.echo('Selection of segment files has started.')
+                typer.echo('=' * 80)
+
+            # サブセットのアノテーションを更新
+            elif current_index < len(subsets):
+
+                # サブセットのアノテーションを更新
+                subset = subsets[current_index]
+                subset.series_title = series_title.strip()
+                subset.episode_number = episode_number.strip()
+                if subset.episode_number == '':
+                    subset.episode_number = None
+                subset.subtitle = subtitle.strip()
+                if subset.subtitle == '':
+                    subset.subtitle = None
+                writer.write(subset.model_dump(mode='json'))
+
+                print(f'番組タイトル: {subset.title}')
+                print(f'番組概要: {subset.description}')
+                print(f'シリーズタイトル: {subset.series_title}')
+                print(f'話数: {subset.episode_number}')
+                print(f'サブタイトル: {subset.subtitle}')
+
+                # 次の処理対象のファイルのインデックスに進める
+                typer.echo('-' * 80)
+                current_index += 1
+
+            # 次の処理対象のファイルがない場合は終了
+            if current_index >= len(subsets):
+                typer.echo('=' * 80)
+                typer.echo('All files processed.')
+                typer.echo('=' * 80)
+                return (
+                    gradio.Textbox(value='アノテーションをすべて完了しました！プロセスを終了してください。', label='ID (読み取り専用)', interactive=False),
+                    gradio.Textbox(value='', label='番組タイトル (読み取り専用)', interactive=False),
+                    gradio.Textbox(value='', label='番組概要 (読み取り専用)', interactive=False),
+                    gradio.Textbox(value='', label='シリーズタイトル', interactive=True),
+                    gradio.Textbox(value='', label='話数 (該当情報がない場合は空欄)', interactive=True),
+                    gradio.Textbox(value='', label='サブタイトル (該当情報がない場合は空欄)', interactive=True),
+                )
+
+            # UI を更新
+            return (
+                gradio.Textbox(value=subsets[current_index].id, label='ID (読み取り専用)', interactive=False),
+                gradio.Textbox(value=subsets[current_index].title, label='番組タイトル (読み取り専用)', interactive=False),
+                gradio.Textbox(value=subsets[current_index].description, label='番組概要 (読み取り専用)', interactive=False),
+                gradio.Textbox(value=subsets[current_index].title, label='シリーズタイトル', interactive=True),
+                gradio.Textbox(value=subsets[current_index].title, label='話数 (該当情報がない場合は空欄)', interactive=True),
+                gradio.Textbox(value=subsets[current_index].title, label='サブタイトル (該当情報がない場合は空欄)', interactive=True),
+            )
+
+        # Gradio UI の定義と起動
+        with gradio.Blocks(css='.gradio-container { max-width: 768px !important; }') as gui:
+            with gradio.Column():
+                gradio.Markdown("""
+                    # EPG データセットサブセットアノテーションツール
+                    Tab キー / Shift + Tab キー を押すと、フォームやボタン間で素早くフォーカスを移動できます。
+                """)
+                id_box = gradio.Textbox(value='確定ボタンを押して、データセット作成を開始してください。', label='ID (読み取り専用)', interactive=False)
+                title_box = gradio.Textbox(value='', label='番組タイトル (読み取り専用)', interactive=False)
+                description_box = gradio.Textbox(value='', label='番組概要 (読み取り専用)', interactive=False)
+                series_title_box = gradio.Textbox(value='', label='シリーズタイトル', interactive=True)
+                episode_number_box = gradio.Textbox(value='', label='話数 (該当情報がない場合は空欄)', interactive=True)
+                subtitle_box = gradio.Textbox(value='', label='サブタイトル (該当情報がない場合は空欄)', interactive=True)
+                with gradio.Row():
+                    confirm_button = gradio.Button('確定', variant='primary')
+                    confirm_button.click(
+                        fn = OnClick,
+                        inputs = [
+                            id_box,
+                            title_box,
+                            description_box,
+                            series_title_box,
+                            episode_number_box,
+                            subtitle_box,
+                        ],
+                        outputs = [
+                            id_box,
+                            title_box,
+                            description_box,
+                            series_title_box,
+                            episode_number_box,
+                            subtitle_box,
+                        ],
+                    )
+
+            # 0.0.0.0:7860 で Gradio UI を起動
+            gui.launch(server_name='0.0.0.0', server_port=7860)
+
 
 if __name__ == '__main__':
     app()
