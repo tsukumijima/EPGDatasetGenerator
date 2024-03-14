7665f3b42d829da3f4b3b7accf2589b6678d11a1
tsukumi <tsukumijima@users.noreply.github.com>
2024-03-14 17:09:44 +0000

    Fix: 細かな修正

--- GenerateEPGDatasetSubset.py
+++ GenerateEPGDatasetSubset.py
@@ -21,13 +21,14 @@ class EPGDatasetSubset(BaseModel):
     duration: int
     title: str
     title_without_symbols: str
-    series_title: str = ''  # 後で自動 or 人力で追加するフィールド
-    episode_number: str | None = None  # 後で自動 or 人力で追加するフィールド
-    subtitle: str | None = None  # 後で自動 or 人力で追加するフィールド
     description: str
     description_without_symbols: str
     major_genre_id: int
     middle_genre_id: int
+    # ここから下は後で自動 or 人力で追加するフィールド
+    series_title: str = ''
+    episode_number: str | None = None
+    subtitle: str | None = None
 
 class EPGDatasetSubsetInternal(EPGDatasetSubset):
     weight: float = 1.0  # 内部でのみ使用
